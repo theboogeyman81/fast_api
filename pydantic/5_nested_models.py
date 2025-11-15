177041e48723aa9e6c63b7f67dde04edbeb5517a
PRATHAM YALIGAR <theboogeyman8144@gmail.com>
2025-11-15 18:53:37 +0530

    nested_model

---
+++ pydantic/5_nested_models.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+
+class Address(BaseModel):
+    city:str
+    state:str
+    pin:str
+
+
+class Patient(BaseModel):
+    name:str
+    gender:str
+    age:int
+    address: Address
+
+address_dict = {'city':'gurgaon' , 'state':'haryana' , 'pin':'122001'}
+
+address1 = Address(**address_dict)
+
+patient_dict = {'name': 'pratham' , 'gender': 'male' , 'age' : 35 , 'address':address1}
+
+patient1 = Patient(**patient_dict)
+
+temp  = patient1.model_dump(include=['name' , 'age' , {'address' : ['state']}] , exclude=['gender'])
+
+print(temp)
+print(type(temp))
+
+
