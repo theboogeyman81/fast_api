ce49d61d2b1509fc705ff882f6008d070c767b5c
PRATHAM YALIGAR <theboogeyman8144@gmail.com>
2025-11-16 08:41:24 +0530

    delete patient

--- main.py
+++ main.py
@@ -157,3 +157,19 @@ def update_patient(patient_id: str , patient_update: PatientUpdate):
 
     return JSONResponse(status_code=200 , content={'message' : 'patient updated'})
 
+
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id:str):
+
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404 , detail = 'Patient not found')
+
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message':'patient deleted'})
+
